bd2cfab4d9f61a63c566210afba22d46db44e136
Henry Malinowski <malinowski.henry@gmail.com>
2018-05-02 13:02:18 -0500

    us lang will be set when returning to lock screen

--- i3/.config/i3/script_lock.sh
+++ i3/.config/i3/script_lock.sh
@@ -4,7 +4,12 @@ if ! [ -x "$(command -v betterlockscreen)" ]; then
     i3-nagbar --type error --message "betterlockscreen is not installed. Cannot lock screen."
     exit 1
 else
+    # don't play music in the background
     mpc pause
     pamixer --mute
+
+    # make sure we aren't locked out because the language is not set to English
+    setxkbmap us
+    setxkbmap -layout us,ru
     betterlockscreen --lock
 fi
